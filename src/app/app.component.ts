b867765d9a46a1ab3fde3ac5bd8727874e3b3d92
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-15 11:50:33 +0800

    Routing not work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
